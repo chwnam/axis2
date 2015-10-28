10d4c2c15a7e6da6c8765e4a816b3ce24982a5c3
changwoo <ep6tri@hotmail.com>
2015-10-28 15:26:36 +0900

    admin notice 삭제.

--- class-base-control.php
+++ class-base-control.php
@@ -71,10 +71,4 @@ class Base_Control {
 
 		return compose_fqn( $app_namespace, $app_name, 'view', $view_slug );
 	}
-
-	public function get_admin_notice() {
-
-		$bootstrap = $this->get_bootstrap();
-		return $bootstrap->get_admin_notice();
-	}
 }
